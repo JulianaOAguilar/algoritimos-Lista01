b5e931ebae7a9a9f4b8fd0b68e2e358c2f690fba
Juliana Aguilar <161730069+JulianaOAguilar@users.noreply.github.com>
2025-03-07 08:52:41 -0300

    Update exercicio4.cpp

--- lista01/exercicio4.cpp
+++ lista01/exercicio4.cpp
@@ -2,12 +2,12 @@
 
 int main() 
 {
-	//4. Receber uma temperatura em Fahrenheits e convertê-la para Celsius.
+	//4. Receber uma temperatura em Fahrenheits e convertÃª-la para Celsius.
 	// Celsius = (Fahrenheit - 32) / 1.8.
 	
 	float fahrenheit, celsius;
 	printf("Insira uma temperatura (em graus fahrenheit): ");
-	scanf("%.2f", &fahrenheit);
+	scanf("%f", &fahrenheit);
 	celsius = (fahrenheit - 32) / 1.8;
 	printf("%.2f fahrenheit equivalem a %.2f graus celsius.", fahrenheit, celsius);
 	return 0;
